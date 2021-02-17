adaca17505424c8f8734f4f7172e470cfb32b2b0
amineouba <63561311+amineouba@users.noreply.github.com>
2021-02-17 14:55:06 +0100

    feat: student/company dao

--- src/main/java/com/mhamed/ProjetJEE/data/CompanyDAO.java
+++ src/main/java/com/mhamed/ProjetJEE/data/CompanyDAO.java
@@ -2,7 +2,11 @@ package com.mhamed.ProjetJEE.data;
 
 import com.mhamed.ProjetJEE.model.Company;
 
+import java.util.List;
+
 public interface CompanyDAO extends UserDAO {
 
     Long save(Company entity);
+
+    List<Company> getAll();
 }
--- src/main/java/com/mhamed/ProjetJEE/data/StudentDAO.java
+++ src/main/java/com/mhamed/ProjetJEE/data/StudentDAO.java
@@ -2,7 +2,11 @@ package com.mhamed.ProjetJEE.data;
 
 import com.mhamed.ProjetJEE.model.Student;
 
+import java.util.List;
+
 public interface StudentDAO extends UserDAO {
 
     Long save(Student entity);
+
+    List<Student> getStudentsBySchoolId(Long schoolId);
 }
